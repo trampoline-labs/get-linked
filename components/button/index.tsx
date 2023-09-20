45bc4d73c8a0daec84fd5ae00c22dcdf2aa54be4
DEE <donzydonathan@gmail.com>
2023-09-20 06:24:22 +0100

    feat: complete responsive ui for navbar component

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -5,7 +5,7 @@ interface ButtonProps {
 
 export default function Button({ label }: ButtonProps) {
   return (
-    <button className="px-14 py-4 text-primary-100 capitalize text-base rounded-sm bg-gradient-to-r from-primary-300 to-primary-400">
+    <button className="px-14 py-4 text-primary-100 capitalize text-base rounded-sm bg-gradient-to-r from-primary-300 via-primary-200 to-primary-400 lg:hover:scale-110 transition-all duration-200">
       {label}
     </button>
   );
--- components/main-nav/index.tsx
+++ components/main-nav/index.tsx
@@ -11,21 +11,31 @@ import {
 
 export default function Navbar() {
   return (
-    <nav className="px-6 py-8 flex justify-between border-b border-b-secondary-100">
+    <nav className="px-6 lg:px-14 py-8 flex justify-between items-center border-b border-b-secondary-100">
       <Link href="/">
-        <h1 className="text-primary-100 font-clash-display font-bold text-xl">
+        <h1 className="text-primary-100 font-clash-display font-bold text-xl lg:text-4xl">
           get<span className="text-primary-200">linked</span>
         </h1>
       </Link>
 
-      <div className="hidden">
-        <ul>
-          <li>timeline</li>
-          <li>overview</li>
+      <div className="hidden lg:flex items-center justify-between gap-24">
+        <ul className="flex gap-10">
           <li>
-            faq<span>s</span>
+            <CustomLink path="/">timeline</CustomLink>
           </li>
-          <li>contact</li>
+          <li>
+            <CustomLink path="/">overview</CustomLink>
+          </li>
+          <li>
+            <CustomLink path="/">
+              <span className="uppercase">
+                faq<span className="lowercase">s</span>
+              </span>
+            </CustomLink>
+          </li>
+          <li>
+            <CustomLink path="/">contact</CustomLink>
+          </li>
         </ul>
 
         <Button label="register" />
@@ -98,7 +108,10 @@ function CustomLink({
 }) {
   return (
     <>
-      <Link href={path} className="capitalize text-primary-100 text-lg">
+      <Link
+        href={path}
+        className="capitalize text-primary-100 text-lg lg:hover:text-transparent lg:hover:bg-gradient-to-r from-primary-400 to-primary-300 lg:bg-clip-text transition-all duration-300 ease-out"
+      >
         {children}
       </Link>
     </>

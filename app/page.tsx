4581b52b0eb3f85393478502910e64fa28f73b11
DEE <donzydonathan@gmail.com>
2023-09-21 22:37:28 +0100

    feat(rules-and-guidelines): complete responsive layout for this section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Hero from "@/components/hero";
+import RulesAndGuideLines from "@/components/rules-and-guidelines";
 import TimeLine from "@/components/timeline";
 
 export default function Home() {
   return (
     <main className="text-white">
       <Hero />
+
+      <RulesAndGuideLines />
 
       <TimeLine />
     </main>

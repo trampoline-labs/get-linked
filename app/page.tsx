71db1ba43249c424ccd64e3cf8bc7949c28a70cd
DEE <donzydonathan@gmail.com>
2023-09-21 22:01:54 +0100

    feat(introduction): complete responsive introduction section in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Hero from "@/components/hero";
+import Introduction from "@/components/intro";
 import TimeLine from "@/components/timeline";
 
 export default function Home() {
   return (
     <main className="text-white">
       <Hero />
+
+      <Introduction />
 
       <TimeLine />
     </main>

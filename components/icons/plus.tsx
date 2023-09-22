a22a5e75fa7032047a7ecb2a578ca688418d9b1b
DEE <donzydonathan@gmail.com>
2023-09-22 01:48:04 +0100

    chore: add plus icon for faqs

---
+++ components/icons/plus.tsx
@@ -0,0 +1,16 @@
+export default function PlusIcon() {
+  return (
+    <svg
+      width="11"
+      height="11"
+      viewBox="0 0 11 11"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4.76376 10.2385V6.43853H0.943765V4.55853H4.76376V0.798525H6.56377V4.55853H10.3838V6.43853H6.56377V10.2385H4.76376Z"
+        fill="#D434FE"
+      />
+    </svg>
+  );
+}

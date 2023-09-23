4b70e4b87c3ca964b7e4d92ead74b986ef4bcbe8
DEE <donzydonathan@gmail.com>
2023-09-23 23:42:47 +0100

    fix some animations and form validation though still broken

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,71 +1,149 @@
-import Image from 'next/image';
-import designer from '@/public/images/image-eight.svg';
-import littlePeople from '@/public/icons/little-man-woman.svg';
-import Button from '@/components/button';
-import Popup from '@/components/ui/popup';
+"use client";
+
+import Image from "next/image";
+import designer from "@/public/images/image-eight.svg";
+import littlePeople from "@/public/icons/little-man-woman.svg";
+import Button from "@/components/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import Popup from "@/components/ui/popup";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { cn } from "@/lib/utils";
+import { API_ENDPOINT } from "@/lib/data";
 
 const forms = [
   {
-    id: 'f1',
-    placeholder: 'Enter the name of your group',
+    id: "f1",
+    placeholder: "Enter the name of your group",
     label: "team's name",
-    type: 'text',
+    type: "text",
+    field: "team_name",
   },
   {
-    id: 'f2',
-    placeholder: 'Enter your phone number',
-    label: 'phone',
-    type: 'number',
+    id: "f2",
+    placeholder: "Enter your phone number",
+    label: "phone",
+    type: "number",
+    field: "phone_number",
   },
   {
-    id: 'f3',
-    placeholder: 'Enter your email address',
-    label: 'email',
-    type: 'email',
+    id: "f3",
+    placeholder: "Enter your email address",
+    label: "email",
+    type: "email",
+    field: "email",
   },
   {
-    id: 'f4',
-    placeholder: 'What is your group project topic',
-    label: 'project topic',
-    type: 'text',
+    id: "f4",
+    placeholder: "What is your group project topic",
+    label: "project topic",
+    type: "text",
+    field: "project_topic",
   },
 ];
 
 const dropdowns = [
   {
-    id: 'd1',
-    label: 'category',
-    name: 'category',
+    id: "d1",
+    label: "category",
+    name: "category",
     options: [
       {
-        label: 'Select your category',
-        value: '',
+        label: "Mobile",
+        value: 1,
+      },
+      {
+        label: "Web",
+        value: 2,
+      },
+      {
+        label: "Backend",
+        value: 3,
       },
     ],
   },
   {
-    id: 'd2',
-    label: 'group size',
-    name: 'group-size',
+    id: "d2",
+    label: "group size",
+    name: "group_size",
     options: [
       {
-        label: 'Select',
-        value: '',
+        label: "2",
+        value: 2,
+      },
+      {
+        label: "4",
+        value: 4,
+      },
+      {
+        label: "6",
+        value: 6,
       },
     ],
   },
 ];
 
+const schema = z
+  .object({
+    team_name: z.string().nonempty("This field is required"),
+    email: z
+      .string()
+      .nonempty("This field is required")
+      .email("Email format is not valid"),
+    phone_number: z.number({
+      required_error: "Phone Number is required",
+      invalid_type_error: "Enter a valid Number",
+    }),
+    project_topic: z.string().nonempty("This field is required"),
+    category: z.enum(["web", "backend", "mobile"]),
+    group_size: z.enum(["2", "4", "6"]),
+  })
+  .required();
+
+type FormData = z.infer<typeof schema>;
+
 export default function RegisterPage() {
+  const form = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      team_name: "",
+      email: "",
+      phone_number: undefined,
+      project_topic: "",
+    },
+  });
+
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+  } = form;
+
+  console.log(errors);
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    if (isValid) {
+      try {
+        const res = (
+          await fetch(`${API_ENDPOINT}/registration`, {
+            method: "post",
+            body: JSON.stringify({ privacy_policy_accepted: true, ...data }),
+          })
+        ).json();
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    console.log(data);
+  };
+
   return (
     <>
-      <Popup />
+      {/* <Popup /> */}
       <main className="py-12 px-4 lg:flex lg:flex-row lg:gap-4 lg:px-10">
         <div className="w-10/12 mx-auto lg:w-auto lg:self-center">
-          <Image
-            src={designer}
-            alt="graphics designer"
-          />
+          <Image src={designer} alt="graphics designer" />
         </div>
 
         <div className="px-7 mt-4 lg:shrink-0 lg:flex-1 lg:basis-[55%] lg:bg-secondary-200 lg:p-20 lg:rounded-md lg:shadow-lg">
@@ -75,12 +153,9 @@ export default function RegisterPage() {
 
           <div className="text-primary-100 lg:mt-10">
             <p className="flex gap-3 text-xs lg:text-sm items-baseline lg:w-2/3">
-              Be part of this movement{' '}
+              Be part of this movement{" "}
               <span className="flex justify-center border-b border-dashed border-primary-200 w-1/3">
-                <Image
-                  src={littlePeople}
-                  alt="movement"
-                />
+                <Image src={littlePeople} alt="movement" />
               </span>
             </p>
             <h1 className="uppercase text-xl tracking-wider mt-2 lg:text-2xl">
@@ -91,20 +166,24 @@ export default function RegisterPage() {
           <form
             action=""
             className="capitalize text-primary-100 text-sm flex flex-col gap-6 mt-4 lg:mt-8"
+            onSubmit={handleSubmit(onSubmit)}
           >
             <div className="grid auto-rows-auto gap-6 lg:grid-rows-2 lg:grid-cols-2 lg:gap-x-8 lg:gap-y-6">
               {forms.map((form) => {
                 return (
-                  <div
-                    className="flex flex-col gap-2"
-                    key={form.id}
-                  >
+                  <div className="flex flex-col gap-2" key={form.id}>
                     <label htmlFor={form.id}>{form.label}</label>
                     <input
                       placeholder={form.placeholder}
                       id={form.id}
                       type={form.type}
-                      className="bg-transparent rounded-md border-primary-100 placeholder:text-sm"
+                      className={cn(
+                        "bg-transparent rounded-md border-primary-100 placeholder:text-sm",
+                        errors[form.field as keyof FormData] &&
+                          "border border-red-500"
+                      )}
+                      {...(register(form.field as keyof FormData),
+                      { valueAsNumber: form.type === "number" ? true : false })}
                     />
                   </div>
                 );
@@ -120,15 +199,15 @@ export default function RegisterPage() {
                   >
                     <label htmlFor={item.id}>{item.label}</label>
                     <select
-                      name={item.name}
                       id={item.id}
                       className="bg-transparent rounded-md border-primary-100 text-sm lg:text-base"
+                      {...register(item.name as keyof FormData)}
                     >
                       {item.options.map((opt, idx) => {
                         return (
                           <option
                             value={opt.value}
-                            className="capitalize text-xs"
+                            className="capitalize text-xs text-black"
                             key={idx}
                           >
                             {opt.label}
